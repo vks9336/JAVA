37de941a08889d6806e6f68b120f22c7c6dbee96
vks9336 <vishalkumar9336108623@gmail.com>
2025-02-19 16:55:48 +0530

    Made  a new node in Doubly Linked List

---
+++ src/com/LinkedList_in_JAVA/DoublyLinkedList/Node.java
@@ -0,0 +1,13 @@
+package com.LinkedList_in_JAVA.DoublyLinkedList;
+
+public class Node {
+    int data;
+    Node prev;
+    Node next;
+
+    public Node(int data) {
+        this.data = data;
+        this.prev = null;
+        this.next = null;
+    }
+}
